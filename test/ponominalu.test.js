8e25031187b87f11bce9f97b402560f8bd57e949
yuri-spb <yuri.sukhorukov.spb@gmail.com>
2020-01-23 17:24:47 +0300

    check title

--- test/ponominalu.test.js
+++ test/ponominalu.test.js
@@ -6,17 +6,15 @@ jasmine.DEFAULT_TIMEOUT_INTERVAL = 999999;
 let browser;
 let page;
 
-const headless = true
+const headless = true;
 
-describe('Google', () => {
+describe('Ponominalu.ru', () => {
   beforeAll(async () => {
     browser = await puppeteer.launch({headless});
     page = await browser.newPage();
   });
 
   it('screenshot of the request "ponominalu" must be saved', async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
     
     const folder = './screenshots';
     const file = 'google-search-ponominalu.png';
@@ -40,8 +38,30 @@ describe('Google', () => {
     })
 
     expect(fs.existsSync(`${folder}/${file}`)).toBe(true);
+  });
+
+  it('compair main and afisha titles', async () => {
+    const url = 'https://ponominalu.ru/';
+
+    await page.setViewport({
+      width: 1280,
+      height: 720
+    });
     
-    await browser.close();
+    await page.goto(url, {timeout: 3000000});
+
+    await page.waitForSelector('title');
+    let title1 = await page.evaluate(el => el.innerHTML, await page.$('title'));
+    console.log(`title ponominalu.ru: ${title1}`);
+
+    await page.waitForSelector('a[href="/afisha"]');
+    await page.$eval('a[href="/afisha"]', el => el.click());
+    
+    await page.waitForNavigation({ waitUntil: 'networkidle0' });
+    let title2 = await page.evaluate(el => el.innerHTML, await page.$('title'));
+    console.log(`title ponominalu.ru/afisha: ${title2}`);
+
+    expect(title1 !== title2).toBe(true);
   });
 
   afterAll(async () => {
