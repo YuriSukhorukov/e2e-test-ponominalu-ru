e3d323deb8b40585729251f9e761ba78dd7ad2f0
yuri-spb <yuri.sukhorukov.spb@gmail.com>
2020-01-23 17:51:17 +0300

    upd

--- test/ponominalu.test.js
+++ test/ponominalu.test.js
@@ -20,9 +20,10 @@ describe('Ponominalu.ru', () => {
     const file = 'google-search-ponominalu.png';
     const url = 'https://google.com';
     const request = 'ponominalu';
+    const selector_input = 'input.gLFyf.gsfi';
 
     await page.goto(url);
-    await page.type('input.gLFyf.gsfi', request);
+    await page.type(selector_input, request);
     await page.keyboard.press('Enter');
     await page.waitForNavigation({ waitUntil: 'networkidle0' });
 
@@ -41,24 +42,28 @@ describe('Ponominalu.ru', () => {
   });
 
   it('compair main and afisha titles', async () => {
-    const url = 'https://ponominalu.ru/';
 
-    await page.setViewport({
+    const url = 'https://ponominalu.ru/';
+    const selector_a = 'a[href="/afisha"]';
+    const selector_title = 'title';
+    const timeout = 3000000
+    const viewport = {
       width: 1280,
       height: 720
-    });
-    
-    await page.goto(url, {timeout: 3000000});
+    };
 
-    await page.waitForSelector('title');
-    let title1 = await page.evaluate(el => el.innerHTML, await page.$('title'));
+    await page.setViewport(viewport);
+    await page.goto(url, {timeout});
+
+    await page.waitForSelector(selector_title);
+    let title1 = await page.evaluate(el => el.innerText, await page.$(selector_title));
     console.log(`title ponominalu.ru: ${title1}`);
 
-    await page.waitForSelector('a[href="/afisha"]');
-    await page.$eval('a[href="/afisha"]', el => el.click());
+    await page.waitForSelector(selector_a);
+    await page.$eval(selector_a, el => el.click());
     
     await page.waitForNavigation({ waitUntil: 'networkidle0' });
-    let title2 = await page.evaluate(el => el.innerHTML, await page.$('title'));
+    let title2 = await page.evaluate(el => el.innerText, await page.$(selector_title));
     console.log(`title ponominalu.ru/afisha: ${title2}`);
 
     expect(title1 !== title2).toBe(true);
